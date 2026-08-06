23c0cf43cc913a78764bcd10e6ba88b954e2a776
unknown <amithaoffical043@gmail.com>
2026-08-06 12:57:23 +0530

    Fix production backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,27 @@
 import axios from "axios";
 
+const backendUrl =
+  import.meta.env.VITE_BACKEND_URL ||
+  "http://127.0.0.1:8000";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: `${backendUrl}/api`,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("foodkindl_access");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(
+      "foodkindl_access"
+    );
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
